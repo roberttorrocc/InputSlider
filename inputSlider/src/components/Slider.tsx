f1f1ffe4b95b2b1e3124955f0853b01396f4241b
roberttorrocc <roberttorro@capitole-consulting.com>
2023-03-09 12:00:53 +0100

    limitador funcional: ya no puden sobrepasar una bola a la otra

--- inputSlider/src/components/Slider.tsx
+++ inputSlider/src/components/Slider.tsx
@@ -9,25 +9,30 @@ function Slider() {
   return (
     <div className="slider">
       <input
-        id="rightBullet"
-        type="range"
-        value={rightValue}
-        onChange={(e)=>setRightValue( Number(e.target.value))}
-        min="0"
-        max="1000"
-        className="bullet bullet--right"
-      />
-      <div className="bullet--line" />
-      <input
         id="leftBullet"
         type="range"
         value={leftValue}
-        min="0"
+        onChange={(e)=> {
+          if(Number(e.target.value) > rightValue){
+            setLeftValue( Number(e.target.value))
+          }}}        min="0"
         max="1000"
-        onChange={(e)=>setLeftValue( Number(e.target.value))}
         className="bullet bullet--left"
       />
-      <p className="nums">{`${rightValue}     ${leftValue}`}</p>
+      <div className="bullet--line" />
+      <input
+        id="rightBullet"
+        type="range"
+        value={rightValue}
+        min="0"
+        max="1000"
+        onChange={(e)=> {
+          if(Number(e.target.value) > leftValue){
+            setRightValue( Number(e.target.value))
+          }}}
+        className="bullet bullet--left"
+      />
+      <p className="nums">{`${leftValue}     ${rightValue}`}</p>
     </div>
   );
 }
